76c27d3cef4acdc706221f1b32586a09d6ddc3c4
yaolin <cherry.figh0918@gmail.com>
2019-11-20 12:51:46 -0500

    Update mainGenerateData.m

--- FUNCTIONS/mainGenerateData.m
+++ FUNCTIONS/mainGenerateData.m
@@ -3,23 +3,23 @@
 
 clear variables
 clear all;
-clc;
+% clc;
 
 
-mID = 1    % method applied, 1 for FFT-Threshold, 2 for DSSSS ,3 for Notch
+mID = 1    % method applied, 1 for FFT-Threshold, 2 for DSSSS ,3 for Notch 
 N = 1e4        % number of bits run per iteration
-Ndat         = [2 20 20 2 1 10]
-% Ndat         = [1 1 1 1 1 100]
-prodNdat = prod(Ndat)
+Ndat = 16e3     % number of dataset size
 
 
-Xname =  strcat('./Data2/X200k',num2str(mID),'.mat')    % name of saved data
-Yname =  strcat('./Data2/Y200k',num2str(mID),'.mat')
+folder = '../DATA/16k-randFreq(100,2k)/'
+
+% Xname =  strcat('./Data5/X16k',num2str(mID),'f.mat')     % name of saved data
+% Yname =  strcat('./Data5/Y16k',num2str(mID),'f.mat')
 
 %====================Add Path==========================%
-addpath(genpath('../'));
-% rand('seed',0);
-% randn('seed',0);
+addpath(genpath('./'));
+rand('seed',0);
+randn('seed',0);
 
 
 
@@ -28,7 +28,7 @@ isTimeOffset = 0;  % random time offset?  0 = no, 1 = yes
 isPhaseOffset = 0; % random phase offset?  0 = no, 1 = yes
 isFreqOffset = 0;  % random frequency offset?  0 = no, 1 = yes
 f_fo_max = 20;    % maximum frequency offset
-NumIter = 1;    % number of iterations to run per JtoS value;
+NumIter = 5;    % number of iterations to run per JtoS value;
 % randomizes impact of frequency offset
 
 %===============Windowing Setting======================%
@@ -55,6 +55,7 @@ chan = 'AWGN';
 %====================Interference Parameters=====================%
 % intParams.type = 'CW';   %'NONE''CW','CHIRP','MOD','FiltNoise'
 INTFERENCE = {'NONE','CW','CHIRP','MOD','FiltNoise'};
+subINTFERENCE = {'NONE','CW'}
 intParams.type = INTFERENCE(1);
 intParams.fc = fd/100;  % center frequency of interference
 intParams.BW = fs/50;   % bandwidth of filtered noise or chirp
@@ -113,174 +114,234 @@ tic;
 
 
 
-V_bitsPerSym = [1,2];
-V_JtoS       = -10 + rand(1,100)*20;
-V_EbNo       = 0+rand(1,100)*20;
-V_IntfType   = [1,2];
-V_duty       = 0.2 + 0.6*rand(1,5);
+isPhaseOffset =0
+isFreqOffset =  0
+isTimeOffset =0
 
-Nc = 0;
-for i1 = 1:Ndat(1)
-    bitsPerSym = V_bitsPerSym(i1);
+
+for k = 1:Ndat
     
-    for i2= 1:Ndat(2)
-        JtoS = V_JtoS(i2);
-        
-        
-        
-        for i3= 1:Ndat(3)
-            EbNo = V_EbNo(i3);
-            
-            for i4= 1:Ndat(4)
-                IntfType = V_IntfType(i4);
-                intParams.type = INTFERENCE(IntfType);
-                
-                for i5= 1:Ndat(5)
-                    duty = V_duty(1);
-                    intParams.duty = duty;
-                    
-                    Nc = Nc+1;
-                    
-                    for i6 = 1:Ndat(6)
-                        
-                        for ii=1:length(JtoS)
-                            
-                            for iii=1:NumIter
-                                % ===============generate TX signal======================%
-                                bits = round(rand(1,N));
-                                %xpsk_bits = Xpsk(bitsPerSym, bits);
-                                %===============Modulation part ======================%
-                                if(strcmp(method,'DsssNbiCancel')||(strcmp(method,'FFT-Thresh') && isDSSS == 1))
-                                    [sig, pn] = psk_modDSSS(beta, span, sps, Ns, bits, bitsPerSym);
-                                else
-                                    [sig,pulse] = psk_mod(bitsPerSym, sps, eBW, bits);
-                                end
-                                
-                                if (strcmp(method,'DsssNbiCancel')||(strcmp(method,'FFT-Thresh') && isDSSS == 1))
-                                    rChan = channel(sig, EbNo, chan, Ns, bitsPerSym);
-                                else
-                                    rChan = channel(sig, EbNo, chan, sps, bitsPerSym);
-                                end
-                                
-                                % add interference
-                                int = addInterf(sig, JtoS(ii), intParams, fs);  %send it sig so we calculate J/S vs signal power, not signal + noise power
-                                r = rChan  + int;
-                                SNRdB =  EbNo - 10*log10(sps) + 10*log10(bitsPerSym);
-                                SNR = 10^(SNRdB/10);
-                                
-                                
-                                %=============Mitigation Part ========================%
-                                
-                                if(strcmp(method,'FFT-Thresh')||strcmp(method,'DsssNbiCancel'))
-                                    
-                                    if (strcmp(method,'DsssNbiCancel')||(strcmp(method,'FFT-Thresh') && isDSSS == 1))
-                                        rClean = FreqDomainNotch(r,2048);
-                                        addLength = sps*N*Ns/bitsPerSym - length(rClean);
-                                        rClean = [rClean, zeros(1,addLength)];
-                                        
-                                    else
-                                        rClean = FreqDomainNotch(r,windowSize);
-                                    end
-                                    
-                                    
-                                    
-                                end
-                                
-                                if(strcmp(method,'DsssNbiCancel'))
-                                    [sig,EstOffset] = time_align_DSSS(r, beta, span, sps, Ns, pn);
-                                    [sig,EstFost] = frequencyCompDsss(sig,sps,pn,fs,checkUpbd, precision, Ns, N,bitsPerSym);
-                                    x_end1 = sig;
-                                    x_end2 = downsample(x_end1,sps);
-                                    x_end3 = windowProcessDsss(rClean,windowSize, qq ,sps,FilterLength,  beta, span, Ns, pn, N, fs,checkUpbd, precision, bitsPerSym);
-                                    
-                                end
-                                
-                                if(strcmp(method,'FFT-Thresh') && isDSSS == 1)
-                                    %==================Frequency offset mitigate========%
-                                    [sig,EstOffset] = time_align_DSSS(r, beta, span, sps, Ns, pn);
-                                    [sig,EstFost] = frequencyCompDsss(sig,sps,pn,fs,checkUpbd, precision, Ns, N,bitsPerSym);
-                                    x_end1 = sig;
-                                    x_end2 = downsample(x_end1,sps);
-                                    [sig,EstOffset] = time_align_DSSS(rClean, beta, span, sps, Ns, pn);
-                                    [sig,EstFost] = frequencyCompDsss(sig,sps,pn,fs,checkUpbd, precision, Ns, N,bitsPerSym);
-                                    x_end1 = sig;
-                                    x_end3 = downsample(x_end1,sps);
-                                    
-                                end
-                                
-                                
-                                
-                                if(strcmp(method,'fFT'))
-                                    Nfft = 10000;
-                                    nBlks = floor(length(r)/Nfft);
-                                    rClean = zeros(1,Nfft*nBlks);
-                                    for i=1:nBlks
-                                        rClean((i-1)*Nfft+1:i*Nfft) = myChirpCancel(r((i-1)*Nfft+1:i*Nfft));
-                                    end
-                                    
-                                end
-                                
-                                
-                                if(strcmp(method,'NotchFilter'))
-                                    rClean = windowProcessNotch(r,PoleRadius,windowSize);
-                                end
-                                
-                                
-                                
-                                % ============check results part========================%
-                                ideal(ii) = q(sqrt(2*10.^(0.1*EbNo)));
-                                
-                                if(strcmp(method,'DsssNbiCancel')||(strcmp(method,'FFT-Thresh') && isDSSS == 1))
-                                    if iii == 1
-                                        unMitBER(ii) = 1/NumIter*psk_demodDSSS(x_end2, pn, Ns, N, bitsPerSym,eBW, bits, 5000, sps, addLength);
-                                        MitBER(ii) = 1/NumIter*psk_demodDSSS(x_end3, pn, Ns, N, bitsPerSym,eBW, bits, 5000, sps, addLength);
-                                    else
-                                        unMitBER(ii) = unMitBER(ii) + 1/NumIter*psk_demodDSSS(x_end2, pn, Ns, N, bitsPerSym,eBW, bits, 5000, sps, addLength);
-                                        MitBER(ii) = MitBER(ii) + 1/NumIter*psk_demodDSSS(x_end3, pn, Ns, N, bitsPerSym,eBW, bits, 5000, sps, addLength);
-                                    end
-                                    
-                                else
-                                    if iii == 1
-                                        unMitBER(ii) = 1/NumIter*psk_demod(r, bitsPerSym, sps, eBW,bits,0);
-                                    else
-                                        unMitBER(ii) = unMitBER(ii) + 1/NumIter*psk_demod(r, bitsPerSym, sps, eBW,bits,0);
-                                    end
-                                    
-                                    if iii == 1
-                                        MitBER(ii) = 1/NumIter* psk_demod(rClean, bitsPerSym, sps, eBW,bits,0);
-                                    else
-                                        MitBER(ii) = MitBER(ii) + 1/NumIter* psk_demod(rClean, bitsPerSym, sps, eBW,bits,0);
-                                    end
-                                    
-                                end
-                                
-                            end % iteration loop
-                        end     % JtoS loop
-                        
-                        Y = [Y;unMitBER * 1000, MitBER * 1000];
-                        X = [X;bitsPerSym, JtoS, EbNo, IntfType,duty ];
-                        
-                        if mod(Nc,100) == 0
-                            Nc
-                            toc;
-                            
-                        end
-                       
-                        
-                    end
-                    
+    
+    % ----- rewrite -----
+    % isPhaseOffset = randi(2)-1;
+    % isFreqOffset = randi(2)-1;
+    % isFreqOffset = randi(2)-1;
+    
+    % intParams.fc = 100 * randi(20);     % default 1e3, rand from  100 - 2e3
+    bitsPerSym = randi(2);
+    sps = 4;  % fix
+    JtoS = -10 + rand*20;
+    EbNo = 0 + rand*20;
+    temp = [1,2];        % ---------------------
+    temp2 = randi(2);
+    IntfType = temp( temp2) ;  
+    intParams.type = subINTFERENCE(temp2);
+    duty = 0.2 + 0.6*rand;  
+    intParams.duty = duty;
 
-                    
-                    
-                    
-                end
+    
+    for ii=1:length(JtoS)
+        
+        for iii=1:NumIter
+            % ===============generate TX signal======================%
+            bits = round(rand(1,N));
+            %xpsk_bits = Xpsk(bitsPerSym, bits);
+            %===============Modulation part ======================%
+            if(strcmp(method,'DsssNbiCancel')||(strcmp(method,'FFT-Thresh') && isDSSS == 1))
+                [sig, pn] = psk_modDSSS(beta, span, sps, Ns, bits, bitsPerSym);
+            else
+                [sig,pulse] = psk_mod(bitsPerSym, sps, eBW, bits);
             end
-        end
+            
+            %============ Channel Simulation part=================%
+            % add phase random offset
+             if isPhaseOffset
+                 po = rand;
+                 sig = sig.*exp(-1j*2*pi*po);  %
+        
+             end
+            
+            % % add random frequency offset
+             if isFreqOffset
+                 f_fo = f_fo_max*rand;
+                 f_fo = f_fo / Ns;       % adjust to lower relative offset for DSSS
+                 sig = sig.* exp(-1j*2*pi*[1:length(sig)]*f_fo/fs);
+                
+             end
+            
+            % % add random time offset
+             if isTimeOffset
+                 offset = floor(rand*30);
+                 sig = [sig(offset+1:end), zeros(1,offset)];
+             else
+                 offset = 0;
+             end
+            
+            % add noise/multipath
+            % adjust noise level for DSSS case since higher samples per bit
+            if (strcmp(method,'DsssNbiCancel')||(strcmp(method,'FFT-Thresh') && isDSSS == 1))
+                rChan = channel(sig, EbNo, chan, Ns, bitsPerSym);
+            else
+                rChan = channel(sig, EbNo, chan, sps, bitsPerSym);
+            end
+            
+            % add interference
+            int = addInterf(sig, JtoS(ii), intParams, fs);  %send it sig so we calculate J/S vs signal power, not signal + noise power
+            r = rChan  + int;
+            SNRdB =  EbNo - 10*log10(sps) + 10*log10(bitsPerSym);
+            SNR = 10^(SNRdB/10);
+            
+            
+            
+            %=============Mitigation Part ========================%
+            
+            if(strcmp(method,'FFT-Thresh')||strcmp(method,'DsssNbiCancel'))
+                
+                if (strcmp(method,'DsssNbiCancel')||(strcmp(method,'FFT-Thresh') && isDSSS == 1))
+                    rClean = FreqDomainNotch(r,2048);
+                    addLength = sps*N*Ns/bitsPerSym - length(rClean);
+                    rClean = [rClean, zeros(1,addLength)];
+                    
+                else
+                    rClean = FreqDomainNotch(r,windowSize);
+                end
+                
+                
+                
+            end
+            
+            if(strcmp(method,'DsssNbiCancel'))
+                [sig,EstOffset] = time_align_DSSS(r, beta, span, sps, Ns, pn);
+                [sig,EstFost] = frequencyCompDsss(sig,sps,pn,fs,checkUpbd, precision, Ns, N,bitsPerSym);
+                x_end1 = sig;
+                x_end2 = downsample(x_end1,sps);
+                x_end3 = windowProcessDsss(rClean,windowSize, qq ,sps,FilterLength,  beta, span, Ns, pn, N, fs,checkUpbd, precision, bitsPerSym);
+                
+            end
+            
+            if(strcmp(method,'FFT-Thresh') && isDSSS == 1)
+                %==================Frequency offset mitigate========%
+                [sig,EstOffset] = time_align_DSSS(r, beta, span, sps, Ns, pn);
+                [sig,EstFost] = frequencyCompDsss(sig,sps,pn,fs,checkUpbd, precision, Ns, N,bitsPerSym);
+                x_end1 = sig;
+                x_end2 = downsample(x_end1,sps);
+                [sig,EstOffset] = time_align_DSSS(rClean, beta, span, sps, Ns, pn);
+                [sig,EstFost] = frequencyCompDsss(sig,sps,pn,fs,checkUpbd, precision, Ns, N,bitsPerSym);
+                x_end1 = sig;
+                x_end3 = downsample(x_end1,sps);
+                
+            end
+            
+            
+            
+            if(strcmp(method,'fFT'))
+                Nfft = 10000;
+                nBlks = floor(length(r)/Nfft);
+                rClean = zeros(1,Nfft*nBlks);
+                for i=1:nBlks
+                    rClean((i-1)*Nfft+1:i*Nfft) = myChirpCancel(r((i-1)*Nfft+1:i*Nfft));
+                end
+                
+            end
+            
+            
+            if(strcmp(method,'NotchFilter'))%MA:  this really needs to be %%switched to operate on windows, right now it takes a gigantic %FFT to find
+                %the interference frequency and then
+                %assumes it never moves.  It's a simple
+                %thing to make it operate on blocks and
+                %estimate the interference frequency
+                %every time, but is it stable to keep
+                %changing the IIR filter? You will need
+                %to record the input sample history and
+                %re-settle the filter feedback each time
+                %you change the pole location
+                rClean = windowProcessNotch(r,PoleRadius,windowSize);
+            end
+            
+            
+            
+            % ============check results part========================%
+            ideal(ii) = q(sqrt(2*10.^(0.1*EbNo)));
+            
+            if(strcmp(method,'DsssNbiCancel')||(strcmp(method,'FFT-Thresh') && isDSSS == 1))
+                if iii == 1
+                    unMitBER(ii) = 1/NumIter*psk_demodDSSS(x_end2, pn, Ns, N, bitsPerSym,eBW, bits, 5000, sps, addLength);
+                    MitBER(ii) = 1/NumIter*psk_demodDSSS(x_end3, pn, Ns, N, bitsPerSym,eBW, bits, 5000, sps, addLength);
+                else
+                    unMitBER(ii) = unMitBER(ii) + 1/NumIter*psk_demodDSSS(x_end2, pn, Ns, N, bitsPerSym,eBW, bits, 5000, sps, addLength);
+                    MitBER(ii) = MitBER(ii) + 1/NumIter*psk_demodDSSS(x_end3, pn, Ns, N, bitsPerSym,eBW, bits, 5000, sps, addLength);
+                end
+                
+            else
+                if iii == 1
+                    unMitBER(ii) = 1/NumIter*psk_demod(r, bitsPerSym, sps, eBW,bits,0);
+                else
+                    unMitBER(ii) = unMitBER(ii) + 1/NumIter*psk_demod(r, bitsPerSym, sps, eBW,bits,0);
+                end
+                
+                if iii == 1
+                    MitBER(ii) = 1/NumIter* psk_demod(rClean, bitsPerSym, sps, eBW,bits,0);
+                else
+                    MitBER(ii) = MitBER(ii) + 1/NumIter* psk_demod(rClean, bitsPerSym, sps, eBW,bits,0);
+                end
+                
+            end
+            
+        end % iteration loop
+    end     % JtoS loop
+    
+    
+    
+
+    
+    Y = [Y;unMitBER * 1000, MitBER * 1000];
+    X = [X;  bitsPerSym,JtoS, EbNo, IntfType,duty ];
+
+    if mod(k,100) == 0
+        k
+        toc;
     end
+    
+    
+    
 end
 
 
+save( strcat(folder,'X.mat')  ,'X');
+save( strcat(folder,'Y.mat')  ,'Y');
 
-save(Xname  ,'X');
-save( Yname  ,'Y');
+
+
+
+Y = -10*log(Y/1000 + 10^(-10)) / log(exp(1));
+
+figure;subplot(2,1,1);
+histogram(Y(:,1));
+title('unmit');
+xlabel('-10log(Y)')
+
+subplot(2,1,2);
+histogram(Y(:,2));
+title('mit');
+xlabel('-10log(Y)')
+
+saveas(gcf,strcat(folder,'hist.png'));
+
+
+
+%         figure
+%         semilogy(JtoS, unMitBER, 'k-x')
+%         hold on
+%         semilogy(JtoS, ideal, 'k-o')
+%         hold on
+%         semilogy(JtoS, MitBER, 'r-o')
+%         xlabel('J/S (dB)')
+%         ylabel('BER')
+%         legend('WIthout Mitigation','Ideal','With Mitigation')
+%         grid on;
+%         axis([JtoS(1) JtoS(end) 1e-5 1])
+%         title(strcat(method,tag))
+%
+%         figName = sprintf(strcat(method,'_',intParams.type,'.png'));
+%         saveas(gcf,figName)
 
